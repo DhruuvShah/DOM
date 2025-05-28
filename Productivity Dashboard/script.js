1ed4fa3d36563d75ae22882fb88c2fc8a5523dfb
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-05-28 15:18:03 +0530

    Added New Projects

---
+++ Productivity Dashboard/script.js
@@ -0,0 +1,99 @@
+function openFeatures() {
+    let allElem = document.querySelectorAll('.elem');
+    let fullElemPage = document.querySelectorAll('.fullElem')
+    let fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
+
+    allElem.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            fullElemPage[elem.id].style.display = 'block'
+        })
+    })
+
+    fullElemPageBackBtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            fullElemPage[back.id].style.display = 'none'
+        })
+    })
+}
+// openFeatures()
+
+function todoList() {
+    var currentTask = []
+
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'))
+    } else {
+        console.log('Task List is empty')
+    }
+
+    function renderTask() {
+        let allTask = document.querySelector('.allTask')
+        let sum = ''
+
+        currentTask.forEach(function (elem, idx) {
+            sum = sum + `<div class="task">
+                        <h5>${elem.task} <span class= ${elem.imp}>Imp</span></h5>
+                        <button id= ${idx}>Mark As Completed</button>
+                    </div>`
+        })
+        allTask.innerHTML = sum
+        localStorage.setItem('currentTask', JSON.stringify(currentTask))
+
+        document.querySelectorAll('.task button').forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                currentTask.splice(btn.id, 1)
+                renderTask()
+            })
+        })
+    }
+    renderTask()
+
+    let form = document.querySelector('.addTask form')
+    let taskInput = document.querySelector('.addTask form #task-input')
+    let taskDetailsInput = document.querySelector('.addTask form textarea')
+    let taskCheckbox = document.querySelector('.addTask form #check')
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckbox.checked
+            }
+        )
+        renderTask()
+        taskCheckbox.checked = false
+        taskInput.value = ''
+        taskDetailsInput.value = ''
+    })
+}
+// todoList()
+
+function dailyPlanner() {
+    let dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+    let dayPlanner = document.querySelector('.day-planner')
+    let hours = Array.from({ length: 18 }, (_, idx) => `${6 + idx}:00 - ${7 + idx}:00`)
+
+
+    let wholeDaySum = ''
+    hours.forEach(function (elem, idx) {
+        let saveData = dayPlanData[idx] || ''
+        wholeDaySum = wholeDaySum + `<div class="day-planner-time">
+    <p>${elem}</p>
+    <input id=${idx} type="text" placeholder="..." value=${saveData}>
+    </div>`
+    })
+
+    dayPlanner.innerHTML = wholeDaySum
+
+
+    let dayPlannerInput = document.querySelectorAll('.day-planner input')
+    dayPlannerInput.forEach(function (elem) {
+        elem.addEventListener('input', function () {
+            dayPlanData[elem.id] = elem.value
+            localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData))
+        })
+    })
+}
+dailyPlanner()
